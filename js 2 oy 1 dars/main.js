20d7487596a81333ccf1fdb0d163969fea70b1f7
Muhammadrizo-799 <ilhomovmuhammadrizo141@gmail.com>
2025-06-17 13:24:48 +0500

    Add files via upload

---
+++ js 2 oy 1 dars/main.js
@@ -0,0 +1,13 @@
+function bc(){
+    body.style.backgroundColor = 'black'
+    body.style.color = 'white'
+}
+let body = document.querySelector("body")
+body.computedStyleMap.background = 'black'
+
+function bc2(){
+    body.style.backgroundColor = 'white'
+    body.style.color = 'black'
+}
+let container = document.querySelector(".container")
+container.computedStyleMap.background = '#C4B5A5'
